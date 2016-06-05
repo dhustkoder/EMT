e6e4e546b92c3d58d8ac043e347455d62139ed0e
dhust <rafaelmoura.dev@gmail.com>
2016-06-05 05:25:08 -0300

    simplified ver

--- OP_INTERPRETATION_2/interpreter.cpp
+++ OP_INTERPRETATION_2/interpreter.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2016 Rafael Moura
-// Demonstration of an 8 bit opcode interpreter and carry
+// Demonstration of an 8 bit opcode interpreter and carry, borrow, sign flag
 // for 16 bit math
 
 // done for -std=c++98
@@ -8,11 +8,12 @@
 #include <stdlib.h> // EXIT_... macros
 #include <stddef.h> // size_t
 #include <stdint.h> // _t types
+#include <inttypes.h> // SCNd16
 #include <fstream>
 #include <vector>
 
-template<class T>
-void print_bits(const T);
+
+void print_bits(const uint8_t);
 
 int main()
 {
@@ -52,17 +53,20 @@ int main()
 	// 04: store next byte in C
 	// 08: add B to A
 	// 10: add C to A
+	// 20: sub B from A
+	// 40: sub C from A
 	// 80: store A to X
+	// 82: clear SF
+	// 84: clear CF
+	// 90: zero registers
 
 
 
-
-	uint8_t CF = 0;
-	uint8_t A=0, B=0, C=0; // 0 - 255
-	uint16_t X=0; // 0 - 65535
+	uint8_t CF = 0, SF = 0;
+	uint8_t A=0, B=0, C=0; // <= 255
+	uint16_t X=0; // > 255
 	
-	bytes.push_back(0x00); // push back a null opcode, prevent crash;
-
+	bytes.push_back(0x00); // push back a null op for offset secutiry;
 	for(size_t i = 0; i < bytes.size(); ++i)
 	{
 		switch(bytes[i])
@@ -71,7 +75,8 @@ int main()
 			case 0x01: A = bytes[++i]; continue;
 			case 0x02: B = bytes[++i]; continue;
 			case 0x04: C = bytes[++i]; continue;
-			
+
+
 			case 0x08: // add B to A with carry
 			{
 				uint16_t res = A + B;
@@ -80,19 +85,55 @@ int main()
 				continue;
 			}
 
-			case 0x10: A += C; continue;
-			case 0x80: X = (CF << 1) | A; continue;
-			default: continue;
+			case 0x10: 
+				A += C; 
+				continue;
+
+			case 0x20: // sub B from A with carry
+				SF = ( A < B ) ? 1 : 0;
+				CF = ( A < B ) ? 0x0 : 0x80;
+				A -= B;
+				continue;
+
+			case 0x40: 
+				A -= C;
+				continue;
+
+			case 0x80:
+				if(SF) X = 0xff00  | A ;
+				else X = (CF << 1) | A;
+				continue;
+
+			case 0x82: 
+				SF=0; 
+				continue; 
+
+			case 0x84: 
+				CF=0; 
+				continue;
+
+			case 0x90: 
+				A=0, B=0, C=0, X=0, SF=0, CF=0;
+				continue;
+
+			default: 
+				printf("unknown opcode $%X\n", bytes[i]);
+				continue;
 		}
 	}
 
-	puts("\n\n\tRESULTS:");
-	printf("A = %3u | B = %3u | C = %3u | X = %5u" , A, B, C, X);
+	puts("\n\tRESULTS:");
+	printf("A = %3u | B = %3u | C = %3u | X = %" SCNd16 "\n" , A, B, C, X);
 
-	printf("\nA = "); print_bits(A);
+	printf("A = "); print_bits(A);
 	printf("\nB = "); print_bits(B); 
 	printf("\nC = "); print_bits(C);
-	printf("\nX = "); print_bits(X);
+	
+	// 16 bits X 
+	printf("\nX = "); 
+	print_bits( (X & 0xff00) >> 8 );
+	putchar(' ');
+	print_bits(X & 0xff);
 	putchar('\n');
 
 
@@ -112,31 +153,17 @@ int main()
 
 
 
-template<class T>
-void print_bits(const T val)
+
+void print_bits(const uint8_t byte)
 {
-	size_t j = 0;
-	const T mask =  0x01 << (( sizeof(T)*8 ) - 1);
-	// create a mask with the T's MSB set.
-	// if T == uint8_t, mask = 0x80, if T == uint16_t, mask = 0x8000
+	size_t j;
 
-	// prints 4 bits and a space, then prints other 4 bits
-	size_t bit;
-
-	do
-	{
-
-		for(bit = 0; bit < 4; ++j, ++bit)
-			printf("%c", (val << j)&mask ? '1' : '0');
+	for(j = 8; j > 4; --j)
+		printf("%u", (byte >> (j-1))&0x01);
 	
-		putchar(' ');
+	putchar(' ');
 		
-		for(; bit < 8; ++j, ++bit)
-			printf("%c", (val << j)&mask ? '1' : '0');
-
-		putchar(' ');
-	
-	} while( j < (sizeof(T)*8) );
-
+	for(; j > 0; --j)
+		printf("%u", (byte >> (j-1))&0x01);
 
 }
