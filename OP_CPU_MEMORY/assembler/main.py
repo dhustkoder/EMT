b8806b0e6ee487b8a88b21d4ed767616ef717ef6
dhust <rafaelmoura.dev@gmail.com>
2016-06-07 06:28:16 -0300

    update

--- OP_CPU_MEMORY/assembler/main.py
+++ OP_CPU_MEMORY/assembler/main.py
@@ -6,17 +6,21 @@ import io
 import sys
 from parser import *
 
+
+
 def main():
 
 	try:
 		file = open(sys.argv[1], "r");
+		lines = file.readlines();
+		file.close();
+		parse(lines);
+
 	except Exception as err:
 		sys.stderr.write("open file failed: " + str(err) + "\n");
-	else:
-		parse(file);
-
 
 	return 0;
+
 
 
 main();
--- OP_CPU_MEMORY/assembler/parser.py
+++ OP_CPU_MEMORY/assembler/parser.py
@@ -8,22 +8,15 @@ import sys
 
 '''
 
-MOVBA
-MOVCA
+MOVA
+
 ADDBA
 ADDCA
 SUBAB
 SUBAC
-LOADA byte
-LOADB byte
-LOADC byte
-LOADMA offset
-LOADMB offset
-LOADMC offset
-LOADMD offset
-STOREA offset
-STOREB offset
-STOREC offset
+LOAD R, byte
+LOADM R, offset
+STORE R, offset
 
 '''
 
@@ -32,74 +25,160 @@ STOREC offset
 
 
 def get_immediate(line):
+
 	try:
-	 	x = int(''.join(ele for ele in line if ele.isdigit()));
+
+		hexIdx = line.find("0x");
+		
+		if hexIdx >= 0:
+			x = int(line[hexIdx:], 16);
+		else:
+	 		x = int(''.join(it for it in line if it.isdigit()));
+
 	except Exception as e:
 		sys.stderr.write("Failed to get immediate value from: " + str(line) + "\n");
-		
+
+
 	else:
 		return x;
 
-def loada(line):
-	return 0xA1 << 8 | get_immediate(line);
-def loadb(line):
-	return 0xB1 << 8 | get_immediate(line);
-def loadc(line):
-	return 0xC1 << 8 | get_immediate(line);
-def loadma(line):
-	return 0xA2 << 8 | get_immediate(line);
-def loadmb(line):
-	return 0xB2 << 8 | get_immediate(line);
-def loadmc(line):
-	return 0xC2 << 8 | get_immediate(line);
-def loadmd(line):
-	return 0xD2 << 8 | get_immediate(line);
-def storea(line):
-	return 0xA4 << 8 | get_immediate(line);
-def storeb(line):
-	return 0xB4 << 8 | get_immediate(line);
-def storec(line):
-	return 0xC4 << 8 | get_immediate(line);
+
+
+
+def RaiseMissingRegId(line):
+	raise Exception("Missing Register identifier: " + str(line));
 
 
 
 
+def mova(opcode):
 
-tokens = [("MOVBA", 0xA000), ("MOVCA", 0xA001),  ("ADDBA", 0xA002), 
-          ("ADDCA", 0xA003), ("SUBAB", 0xA004),  ("SUBAC", 0xA005), 
-          ("LOADA", loada),  ("LOADB", loadb),   ("LOADC", loadc), 
-          ("LOADMA",loadma), ("LOADMB", loadmb), ("LOADMC", loadmc),
-          ("LOADMD",loadmd), ("STOREA", storea), ("STOREB",storeb), ("STOREC", storec)];
+	if opcode.find("B") >= 3:
+		return 0xA000;
+	if opcode.find("C") >= 3:
+		return 0xA001;
 
-
-
-
-def decode(tokenSecond, line):
-	print(hex(tokenSecond(line)));
-	return;
+	RaiseMissingRegId(opcode);
 
 
 
 
 
 
+def adda(opcode):
 
-def parse(file):
-	try:	
-		lines = file.readlines();
+	if opcode.find("B") >= 0:
+		return 0xA002;
+	if opcode.find("C") >= 0:
+		return 0xA003;
+
+	RaiseMissingRegId(opcode);
+
+
+
+def suba(opcode):
+
+	if opcode.find("B") >= 0:
+		return 0xA004;
+	if opcode.find("C") >= 0:
+		return 0xA005;
+
+	RaiseMissingRegId(opcode);
+
+
+
+
+def load(opcode):
+
+	line = str(opcode)[3:];
+
+	if line.find("A,") >= 0:
+		return 0xA1 << 8 | get_immediate(line);
+	if line.find("B") >= 0:
+		return 0xB1 << 8 | get_immediate(line);
+	if line.find("C") >= 0:
+		return 0xC1 << 8 | get_immediate(line);
+
+	RaiseMissingRegId(opcode);
+
+
+
+
+def loadm(opcode):
+
+	line = str(opcode)[4:];
+
+	if line.find("A") >= 0:
+		return 0xA2 << 8 | get_immediate(line);
+	if line.find("B") >= 0:
+		return 0xB2 << 8 | get_immediate(line);
+	if line.find("C") >= 0:
+		return 0xC2 << 8 | get_immediate(line);
+	if line.find("D") >= 0:
+		return 0xD2 << 8 | get_immediate(line);
+
+
+	RaiseMissingRegId(opcode);
+
+
+
+def store(opcode):
+
+	line = str(opcode)[5:];
+
+	if line.find("A") >= 0:
+		return 0xA4 << 8 | get_immediate(line);
+	if line.find("B") >= 0:
+		return 0xB4 << 8 | get_immediate(line);
+	if line.find("C") >= 0:
+		return 0xC4 << 8 | get_immediate(line);
+
+
+	RaiseMissingRegId(opcode);
+
+
+tokens = [("MOVA", mova), ("ADDA", adda), ("SUBA", suba), 
+          ("LOAD", load), ("LOADM", loadm), ("STORE", store)];
+
+
+
+
+def decode(tokenSecond, line, ofile):
+	binary = tokenSecond(line);
+	print(hex(binary));
+	ofile.write(bin(binary)[2:]);
+
+
+
+
+
+
+
+def parse(lines):	
+
+	try:
+		ofile = open("output.bin", "wb");
+
 		for line in lines:
-			if not line.strip(): 
+
+			if not str(line.strip()): 
 				continue;
-			found = 0;
-			for a,b in tokens:
-				x = line.find(a);
-				if x >= 0:
-					decode(b, line);
-					found = 1;
+
+			if line[0] == "#":
+				continue;
+		
+
+			for a, b in tokens:
+				x = line.find(' ');
+				if line[:x] == a:
+					decode(b, line, ofile);
 					break;
-					
 	except Exception as err:
 		sys.stderr.write(str(err) + "\n");
+	
+	finally:
+		ofile.close();
+
 
 	return;
 
